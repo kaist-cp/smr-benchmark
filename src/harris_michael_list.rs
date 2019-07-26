c9241c41609ba6f58eb79a6ea549b37fa7104f7a
Jeehoon Kang <jeehoon.kang@kaist.ac.kr>
2019-07-26 11:53:46 +0900

    Port Harris-Michael list to PEBR

--- src/harris_michael_list.rs
+++ src/harris_michael_list.rs
@@ -1,19 +1,20 @@
 use crate::concurrent_map::ConcurrentMap;
-use crossbeam_epoch::{unprotected, Atomic, Guard, Owned, Shared};
+use crossbeam_epoch::{unprotected, Atomic, Guard, Owned, Pointer, Shared, Shield};
 
-use std::mem::ManuallyDrop;
+use std::mem::{self, ManuallyDrop};
 use std::ptr;
 use std::sync::atomic::Ordering;
+use std::ops::Deref;
 
 #[derive(Debug)]
 struct Node<K, V> {
-    key: K,
-
-    value: ManuallyDrop<V>,
-
     // Mark: tag()
     // Tag: not needed
     next: Atomic<Node<K, V>>,
+
+    key: K,
+
+    value: ManuallyDrop<V>,
 }
 
 pub struct List<K, V> {
@@ -46,9 +47,37 @@ impl<K, V> Drop for List<K, V> {
 }
 
 struct Cursor<'g, K, V> {
-    prev: &'g Atomic<Node<K, V>>,
-    curr: Shared<'g, Node<K, V>>,
+    prev: Shield<Node<K, V>>,
+    curr: Shield<Node<K, V>>,
     next: Shared<'g, Node<K, V>>,
+}
+
+impl<'g, K, V> Cursor<'g, K, V> {
+    fn new(prev: Shared<'g, Node<K, V>>, curr: Shared<'g, Node<K, V>>, guard: &Guard) -> Self {
+        Self {
+            prev: Shield::new(prev, guard),
+            curr: Shield::new(curr, guard),
+            next: Shared::null(),
+        }
+    }
+}
+
+struct VRef<K, V> {
+    shield: Shield<Node<K, V>>,
+}
+
+impl<K, V> VRef<K, V> {
+    fn new(shield: Shield<Node<K, V>>) -> Self {
+        Self { shield }
+    }
+}
+
+impl<K, V> Deref for VRef<K, V> {
+    type Target = V;
+
+    fn deref(&self) -> &Self::Target {
+        &unsafe { self.shield.deref() }.value
+    }
 }
 
 impl<K, V> List<K, V>
@@ -64,11 +93,13 @@ where
     /// Returns (1) whether it found an entry, and (2) a cursor.
     #[inline]
     fn find_inner<'g>(&'g self, key: &K, guard: &'g Guard) -> Result<(bool, Cursor<'g, K, V>), ()> {
-        let mut cursor = Cursor {
-            prev: &self.head,
-            curr: self.head.load(Ordering::Acquire, guard),
-            next: Shared::null(),
-        };
+        let mut cursor = Cursor::new(
+            // HACK(@jeehoonkang): we're unsafely assuming the first 8 bytes of both `Node<K, V>`
+            // and `List<K, V>` are `Atomic<Node<K, V>>`.
+            unsafe { Shared::from_usize(&self.head as *const _ as usize) },
+            self.head.load(Ordering::Acquire, guard),
+            guard,
+        );
 
         loop {
             let curr_node = match unsafe { cursor.curr.as_ref() } {
@@ -76,7 +107,11 @@ where
                 Some(c) => c,
             };
 
-            if cursor.prev.load(Ordering::Acquire, guard) != cursor.curr.with_tag(0) {
+            if unsafe { cursor.prev.deref() }
+                .next
+                .load(Ordering::Acquire, guard)
+                != cursor.curr.shared().with_tag(0)
+            {
                 return Err(());
             }
 
@@ -87,19 +122,19 @@ where
                 if curr_key >= key {
                     return Ok((curr_key == key, cursor));
                 }
-                cursor.prev = &curr_node.next;
+                mem::swap(&mut cursor.prev, &mut cursor.curr);
             } else {
-                match cursor.prev.compare_and_set(
-                    cursor.curr.with_tag(0),
+                match unsafe { cursor.prev.deref() }.next.compare_and_set(
+                    cursor.curr.shared().with_tag(0),
                     cursor.next.with_tag(0),
                     Ordering::AcqRel,
                     guard,
                 ) {
                     Err(_) => return Err(()),
-                    Ok(_) => unsafe { guard.defer_destroy(cursor.curr) },
+                    Ok(_) => unsafe { guard.defer_destroy(cursor.curr.shared()) },
                 }
             }
-            cursor.curr = cursor.next;
+            cursor.curr.defend(cursor.next, guard);
         }
     }
 
@@ -111,11 +146,11 @@ where
         }
     }
 
-    pub fn get<'g>(&'g self, key: &K, guard: &'g Guard) -> Option<&'g V> {
+    pub fn get<'g>(&'g self, key: &K, guard: &'g Guard) -> Option<VRef<K, V>> {
         let (found, cursor) = self.find(key, guard);
 
         if found {
-            unsafe { cursor.curr.as_ref().map(|n| &*n.value) }
+            Some(VRef::new(cursor.curr))
         } else {
             None
         }
@@ -129,16 +164,19 @@ where
         });
 
         loop {
+            // TODO: create cursor in this function.
             let (found, cursor) = self.find(&node.key, &guard);
             if found {
                 return false;
             }
 
-            node.next.store(cursor.curr, Ordering::Relaxed);
-            match cursor
-                .prev
-                .compare_and_set(cursor.curr, node, Ordering::AcqRel, &guard)
-            {
+            node.next.store(cursor.curr.shared(), Ordering::Relaxed);
+            match unsafe { cursor.prev.deref() }.next.compare_and_set(
+                cursor.curr.shared(),
+                node,
+                Ordering::AcqRel,
+                &guard,
+            ) {
                 Ok(_) => return true,
                 Err(e) => node = e.new,
             }
@@ -168,11 +206,13 @@ where
                 continue;
             }
 
-            match cursor
-                .prev
-                .compare_and_set(cursor.curr, cursor.next, Ordering::AcqRel, &guard)
-            {
-                Ok(_) => unsafe { guard.defer_destroy(cursor.curr) },
+            match unsafe { cursor.prev.deref() }.next.compare_and_set(
+                cursor.curr.shared(),
+                cursor.next,
+                Ordering::AcqRel,
+                &guard,
+            ) {
+                Ok(_) => unsafe { guard.defer_destroy(cursor.curr.shared()) },
                 Err(_) => {
                     self.find(key, &guard);
                 }
