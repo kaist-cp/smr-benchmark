28e5f69759a0daf8bc85c383b5426375ed468a63
Jaehwang Jerry Jung <tomtomjhj@gmail.com>
2019-08-01 20:17:01 +0900

    features.profiling for jemalloc heap profile

--- bench.py
+++ bench.py
@@ -8,8 +8,13 @@ threadss = [1] + list(range(5, 101, 5))
 
 subprocess.run(['git', 'submodule', 'update', '--init'])
 
+# througput
 for ds in dss:
     for mm in mms:
         for threads in map(str, threadss):
-            subprocess.run(
-                ['cargo', 'run', '--release', '--', '-d', ds, '-m', mm, '-t', threads])
+            subprocess.run(['cargo', 'run', '--release', '--'] +
+                           ['-d', ds, '-m', mm, '-t', threads])
+
+# max mem usage
+# export JEMALLOC_SYS_WITH_MALLOC_CONF=prof:true,prof_gdump:true
+# --features profiling
