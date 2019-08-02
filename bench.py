3859f752170daf59742cb9d911eca282f9668c1b
Jaehwang Jerry Jung <tomtomjhj@gmail.com>
2019-08-02 09:02:39 +0000

    revise run script

--- bench.py
+++ bench.py
@@ -4,32 +4,41 @@ import subprocess
 
 dss = ['List', 'HashMap', 'NMTree', 'BonsaiTree']
 mms = ['EBR', 'PEBR', 'NR']
-threadss = [1] + list(range(5, 101, 5))
+gs = ['', '-g', '-gg']
+ns = ['', '-n', '-nn']
+ts = [1] + list(range(5, 101, 5))
 
 subprocess.run(['git', 'submodule', 'update', '--init'])
 
+run_cmd = ['cargo', 'run', '--release', '--']
+
+
+def opts(ds, mm, t, g='', n=''):
+    return ['-d', ds, '-m', mm, '-t', t
+            ] + ([] if g == '' else [g]) + ([] if n == '' else [n])
+
+
 # througput: less contention
 for ds in dss:
-    for mm in mms:
-        for threads in map(str, threadss):
-            subprocess.run(['cargo', 'run', '--release', '--'] +
-                           ['-d', ds, '-m', mm, '-t', threads])
+    for g in gs:
+        for mm in mms:
+            for t in map(str, ts):
+                subprocess.run(run_cmd + opts(ds, mm, t, g=g))
+                # print(' '.join(run_cmd + opts(ds, mm, t, g=g)))
+
+# non-cooperative t (don't test NR)
+for ds in dss:
+    for n in ['-n', '-nn']:
+        for mm in ['EBR', 'PEBR']:
+            for t in map(str, ts):
+                subprocess.run(run_cmd + opts(ds, mm, t, n=n))
+                # print(' '.join(run_cmd + opts(ds, mm, t, n=n)))
 
 # througput: high contention
-for ds in dss:
-    for mm in mms:
-        for threads in map(str, threadss):
-            subprocess.run(
-                ['cargo', 'run', '--release', '--'] +
-                ['-d', ds, '-m', mm, '-t', threads, '-r100', '-p50'])
-
-# non-cooperative threads (don't test NR)
-for ds in dss:
-    for mm in ['EBR', 'PEBR']:
-        for threads in map(str, threadss):
-            subprocess.run(
-                ['cargo', 'run', '--release', '--'] +
-                ['-d', ds, '-m', mm, '-t', threads, '-n1,1024'])
+# for ds in dss:
+#     for mm in mms:
+#         for t in map(str, ts):
+#             subprocess.run(run_cmd + opts(ds, mm, t, g, n) + ['-r100', '-p50'])
 
 # max mem usage
 # export JEMALLOC_SYS_WITH_MALLOC_CONF=prof:true,prof_gdump:true
