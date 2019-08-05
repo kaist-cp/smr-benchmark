f67f99ab5bad9a47209e210afbc473c8217b33f0
Jaehwang Jerry Jung <tomtomjhj@gmail.com>
2019-08-05 19:57:26 +0900

    plot script

--- plot.py
+++ plot.py
@@ -6,43 +6,51 @@ pd.set_option('display.max_rows', None)
 
 THREADS = "threads"
 THROUGHPUT = "throughput"
-SMR_AND_INTERFERENCE = "SMR & interference"
+PEAK_MEM = "peak_mem"
+AVG_MEM = "avg_mem"
+
+SMR_ONLY = "SMR"
+SMR_I = "SMR & interference"
+SMR_OPCS = "SMR & ops / CS"
+
 LIST = "List"
 HASHMAP = "HashMap"
 NMTREE = "NMTree"
 BONSAITREE = "BonsaiTree"
+
 EBR = "EBR"
 PEBR = "PEBR"
 NR = "NR"
+
 N10MS = ', 10ms'
 STALLED = ', stalled'
 
+OPCS_1 = ', 1 op / CS'
+OPCS_4 = ', 4 ops / CS'
+
 dss = [LIST, HASHMAP, NMTREE, BONSAITREE]
 mms = [EBR, PEBR, NR]
-gs = [0, 2]
+# gs = [0, 2]
 ns = [1, 2]
 ts = [1] + list(range(5, 76, 5))
 
 g_map = {0: 'write-dominated', 2: 'read-dominated'}
 n_map = {0: '', 1: N10MS, 2: STALLED}
+c_map = {1: OPCS_1, 4: OPCS_4}
 
-# TODO: SMR_AND_INTERFERENCE |-> style
-shapes = ['o', '^', 's', 'D', '.', 'x', '*']
-colors = ['b', 'g', 'r', 'c', 'm', 'k', '#FFB86F']
-linetypes = [
-    '-',  # NR
-    ':',  # EBR
-    '--'  # PEBR
-]
 
 line_shapes = {
     NR: '',
     EBR: '.',
     EBR + N10MS: 'o',
     EBR + STALLED: 's',
+    EBR + OPCS_1: 'o',
+    EBR + OPCS_4: 's',
     PEBR: '^',
     PEBR + N10MS: 'D',
     PEBR + STALLED: '*',
+    PEBR + OPCS_1: 'D',
+    PEBR + OPCS_4: '*',
 }
 
 line_colors = {
@@ -50,9 +58,13 @@ line_colors = {
     EBR: 'b',
     EBR + N10MS: 'c',
     EBR + STALLED: 'g',
+    EBR + OPCS_1: 'c',
+    EBR + OPCS_4: 'g',
     PEBR: 'r',
     PEBR + N10MS: 'm',
     PEBR + STALLED: '#FFB86F',
+    PEBR + OPCS_1: 'm',
+    PEBR + OPCS_4: '#FFB86F',
 }
 
 line_types = {
@@ -60,43 +72,85 @@ line_types = {
     EBR: '--',
     EBR + N10MS: '--',
     EBR + STALLED: '--',
+    EBR + OPCS_1: '--',
+    EBR + OPCS_4: '--',
     PEBR: ':',
     PEBR + N10MS: ':',
     PEBR + STALLED: ':',
+    PEBR + OPCS_1: ':',
+    PEBR + OPCS_4: ':',
 }
 
-valid_smri = list(line_shapes.keys())
+valid_line_name = list(line_shapes.keys())
+smrs = [NR, EBR, PEBR]
 
 
-def draw(name, data):
+# line_name: SMR, SMR_OPCS, SMR_I
+def draw(name, data, line_name, y_label, y_value, y_max=None):
     p = ggplot(
             data,
-            aes(x=THREADS, y=THROUGHPUT,
-                color=SMR_AND_INTERFERENCE, shape=SMR_AND_INTERFERENCE, linetype=SMR_AND_INTERFERENCE)) + \
-        geom_line() + xlab('Threads') + ylab('Throughput (M op/s)') + geom_point(size=4) + \
-        scale_shape_manual(line_shapes) + scale_color_manual(line_colors) + scale_linetype_manual(line_types) + \
-        theme_bw()
-
+            aes(x=THREADS, y=y_value,
+                color=line_name, shape=line_name, linetype=line_name)) + \
+        geom_line() + xlab('Threads') + ylab(y_label) + geom_point(size=4) + \
+        scale_shape_manual(line_shapes, na_value='x') + \
+        scale_color_manual(line_colors, na_value='y') + \
+        scale_linetype_manual(line_types, na_value='-.') + \
+        theme_bw() + scale_x_continuous(breaks=ts)
+    if y_max:
+        p += ylim(0, y_max)
     p.save(name, width=8, height=5.5, units="in")
 
 
+# preprocess
 raw_data = {}
 for ds in dss:
     data = pd.read_csv(ds + '_results.csv')
+
     data.throughput = data.throughput.map(lambda x: x / 1000000)
-    data[SMR_AND_INTERFERENCE] = data.mm.map(str) + data.non_coop.map(n_map)
-    data = data[data[SMR_AND_INTERFERENCE].isin(valid_smri)]
+    data.peak_mem = data.peak_mem.map(lambda x: x / 1000000)
+    data.avg_mem = data.avg_mem.map(lambda x: x / 1000000)
+
+    # NOTE: need to filter out rows with -n and -c options
+    data[SMR_ONLY] = data.mm.map(str)
+
+    data[SMR_I] = data.mm.map(str) + data.non_coop.map(n_map)
+    data = data[data[SMR_I].isin(valid_line_name)]
+
+    # NR, EBR c1/c4, PEBR c1/c4
+    data[SMR_OPCS] = data.mm.map(str) + data.ops_per_cs.map(c_map)
+    def rename_smr_opcs(old):
+        if old == NR + OPCS_1:
+            return NR
+        return old
+    data[SMR_OPCS] = data[SMR_OPCS].map(rename_smr_opcs)
+    data = data[data[SMR_OPCS].isin(valid_line_name)]
+
     raw_data[ds] = data.copy()
 
 # 1. 4(DS) throughput graphs, 3 lines (SMR) each. for HashMap, both c1/c4
-# 3. 4 (DS) peak mem graph, 7 lines (SMR & interference)
-
 for ds in dss:
-    for g in gs:
-        data = raw_data[ds].copy()
-        data = data[data.get_rate == g]
-        draw(f'{ds}_g{g}_throughput.pdf', data[data.ops_per_cs == 1])
-        if ds == "HashMap":
-            draw(f'HashMap_g{g}_c4_throughput.pdf', data[data.ops_per_cs == 4])
-        # with pd.option_context('display.max_rows', None):
-        #     print(data)
+    data = raw_data[ds].copy()
+    data = data[data.non_coop == 0]
+    if ds == HASHMAP:
+        draw(f'{ds}_throughput.pdf', data, SMR_OPCS, 'Throughput (M op/s)', THROUGHPUT)
+    else:
+        # colum SMR_ONLY may 
+        draw(f'{ds}_throughput.pdf', data, SMR_ONLY, 'Throughput (M op/s)', THROUGHPUT)
+
+# TODO: adjust y range
+# 3. 4 (DS) peak mem graph, 7 lines (SMR & interference), c4 for HashMap, c1 otherwise
+for ds in dss:
+    data = raw_data[ds].copy()
+    if ds == HASHMAP:
+        # use c4 except for NR
+        data = data[(data.ops_per_cs == 4) | (data.mm == NR)]
+    else:
+        data = data[data.ops_per_cs == 1]
+    y_max = None
+    if ds != LIST:
+        max_except_explosion = data[~data[SMR_I].isin([NR, EBR+STALLED])][data.ds == ds][data.threads == 1]
+    draw(f'{ds}_peak_mem.pdf', data, SMR_I, 'Peak memory usage (MB)', PEAK_MEM, y_max)
+
+# for ds in dss:
+#     data = raw_data[ds].copy()
+#     draw(f'{ds}_avg_mem.pdf', data, SMR_I, 'Average memory usage (MB)', AVG_MEM)
