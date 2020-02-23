200c90ae5f7058f15786b15cfac53f57c9874ae9
Jaehwang Jerry Jung <tomtomjhj@gmail.com>
2020-02-23 14:05:25 +0000

    remove UnsafeCell from shield_pool

--- src/pebr/shield_pool.rs
+++ src/pebr/shield_pool.rs
@@ -1,11 +1,10 @@
 use core::ops::Deref;
 use crossbeam_pebr::{Guard, Shared, Shield, ShieldError};
-use std::cell::UnsafeCell;
 
 /// Thread-local pool of shields
 #[derive(Debug)]
 pub struct ShieldPool<T> {
-    shields: Vec<*mut UnsafeCell<Shield<T>>>,
+    shields: Vec<*mut Shield<T>>,
     /// Indices of available shields in `shields`.
     available: Vec<usize>,
 }
@@ -24,11 +23,11 @@ impl<T> ShieldPool<T> {
         guard: &Guard,
     ) -> Result<ShieldHandle<T>, ShieldError> {
         if let Some(index) = self.available.pop() {
-            let shield_ref = unsafe { &mut *(**self.shields.get_unchecked(index)).get() };
+            let shield_ref = unsafe { &mut **self.shields.get_unchecked(index) };
             shield_ref.defend(ptr, guard)?;
             return Ok(ShieldHandle { pool: self, index });
         }
-        let new_shield = Box::into_raw(Box::new(UnsafeCell::new(Shield::new(ptr, guard)?)));
+        let new_shield = Box::into_raw(Box::new(Shield::new(ptr, guard)?));
         let index = self.shields.len();
         self.shields.push(new_shield);
         Ok(ShieldHandle { pool: self, index })
@@ -47,7 +46,7 @@ impl<T> Drop for ShieldHandle<T> {
     fn drop(&mut self) {
         let pool = unsafe { &mut *self.pool };
         // release only
-        unsafe { (*(**pool.shields.get_unchecked(self.index)).get()).release() };
+        unsafe { (**pool.shields.get_unchecked(self.index)).release() };
         pool.available.push(self.index);
     }
 }
@@ -55,6 +54,6 @@ impl<T> Drop for ShieldHandle<T> {
 impl<T> Deref for ShieldHandle<T> {
     type Target = Shield<T>;
     fn deref(&self) -> &Self::Target {
-        unsafe { &*(**(*self.pool).shields.get_unchecked(self.index)).get() }
+        unsafe { &(**(*self.pool).shields.get_unchecked(self.index)) }
     }
 }
