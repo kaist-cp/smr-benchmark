5b4442c6293a4c0d35b0059bd508ffd3f3702867
Jaehwang Jung <jaehwang.jung@cp.kaist.ac.kr>
2023-01-08 18:26:10 +0900

    epoched barrier to reduce hazards
    HashMap: HP, 20 threads, n0, c1, g0
    ops/s: 36664872, peak mem: 7656200, avg_mem: 5291948
    
    HashMap: HP_PP, 20 threads, n0, c1, g0
    ops/s: 32364756, peak mem: 9225352, avg_mem: 5548877
    
    7.5× hazards → 3.3× hazards

--- hp_pp/src/domain.rs
+++ hp_pp/src/domain.rs
@@ -1,15 +1,20 @@
+use core::sync::atomic::{AtomicUsize, Ordering};
+
 use rustc_hash::FxHashSet;
 
-use crate::{hazard::ThreadRecords, thread::Thread};
+use crate::hazard::ThreadRecords;
+use crate::thread::Thread;
 
 pub struct Domain {
     pub(crate) threads: ThreadRecords,
+    pub(crate) barrier: EpochBarrier,
 }
 
 impl Domain {
     pub const fn new() -> Self {
         Self {
             threads: ThreadRecords::new(),
+            barrier: EpochBarrier(AtomicUsize::new(0)),
         }
     }
 
@@ -19,5 +24,38 @@ impl Domain {
             .flat_map(|thread| thread.iter(reclaimer))
             .filter(|p| !p.is_null())
             .collect()
+    }
+}
+
+pub(crate) struct EpochBarrier(AtomicUsize);
+
+impl EpochBarrier {
+    pub(crate) fn barrier(&self) -> usize {
+        let epoch = self.0.load(Ordering::Acquire);
+        membarrier::heavy();
+        let new_epoch = epoch.wrapping_add(1);
+        match self
+            .0
+            .compare_exchange(epoch, new_epoch, Ordering::Release, Ordering::Acquire)
+        {
+            Ok(_) => new_epoch,
+            Err(new) => new,
+        }
+    }
+
+    pub(crate) fn read(&self) -> usize {
+        let mut epoch = self.0.load(Ordering::Acquire);
+        loop {
+            membarrier::light_membarrier();
+            let new_epoch = self.0.load(Ordering::Acquire);
+            if epoch == new_epoch {
+                return epoch;
+            }
+            epoch = new_epoch
+        }
+    }
+
+    pub(crate) fn check(old: usize, new: usize) -> bool {
+        new.wrapping_sub(old) >= 2
     }
 }
