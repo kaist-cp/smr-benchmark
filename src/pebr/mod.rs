d3c6771583425d7cdfc40e4581191b03f648c608
Jeonghyeon Kim <jeonghyeon.kim@cp.kaist.ac.kr>
2023-03-06 04:35:26 +0000

    Enable PEBR and implement EFRBTree with PEBR

--- src/pebr/mod.rs
+++ src/pebr/mod.rs
@@ -3,6 +3,7 @@ pub mod shield_pool;
 pub mod concurrent_map;
 
 pub mod bonsai_tree;
+pub mod ellen_tree;
 pub mod list;
 pub mod michael_hash_map;
 pub mod natarajan_mittal_tree;
@@ -10,6 +11,7 @@ pub mod natarajan_mittal_tree;
 pub use self::concurrent_map::ConcurrentMap;
 
 pub use self::bonsai_tree::BonsaiTreeMap;
+pub use self::ellen_tree::EFRBTree;
 pub use self::list::{HHSList, HList, HMList};
 pub use self::michael_hash_map::HashMap;
 pub use self::natarajan_mittal_tree::NMTreeMap;
