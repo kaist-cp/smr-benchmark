a1d65592738f83261bf08001ad559fdb780f1afe
Jeonghyeon Kim <jeonghyeon.kim@cp.kaist.ac.kr>
2023-04-14 12:28:04 +0000

    Implement SkipList with CDRC EBR

--- src/cdrc/mod.rs
+++ src/cdrc/mod.rs
@@ -3,9 +3,11 @@ pub mod concurrent_map;
 pub mod list;
 pub mod michael_hash_map;
 pub mod natarajan_mittal_tree;
+pub mod skip_list;
 
 pub use self::concurrent_map::ConcurrentMap;
 
 pub use self::list::{HHSList, HList, HMList};
 pub use self::michael_hash_map::HashMap;
 pub use self::natarajan_mittal_tree::NMTreeMap;
+pub use self::skip_list::SkipList;
