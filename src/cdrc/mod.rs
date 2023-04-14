3783ac0d94d6b8fd357355f8ed34f50143231ef3
Jeonghyeon Kim <jeonghyeon.kim@cp.kaist.ac.kr>
2023-04-14 10:52:00 +0000

    Implement NMTree with CDRC EBR

--- src/cdrc/mod.rs
+++ src/cdrc/mod.rs
@@ -2,8 +2,10 @@ pub mod concurrent_map;
 
 pub mod list;
 pub mod michael_hash_map;
+pub mod natarajan_mittal_tree;
 
 pub use self::concurrent_map::ConcurrentMap;
 
 pub use self::list::{HHSList, HList, HMList};
 pub use self::michael_hash_map::HashMap;
+pub use self::natarajan_mittal_tree::NMTreeMap;
