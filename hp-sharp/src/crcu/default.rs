759d5ec680f17d31941aadce40357bf2fe9d85ab
Jeonghyeon Kim <jeonghyeon.kim@cp.kaist.ac.kr>
2023-06-27 10:41:42 +0000

    Remove default `Global` in CRCU

--- hp-sharp/src/crcu/default.rs
+++
@@ -1,9 +0,0 @@
-use super::{Global, Handle};
-
-/// A default [`Global`] domain for CRCU.
-pub static GLOBAL: Global = Global::new();
-
-thread_local! {
-    /// A default thread-local `Handle` attached to [`Global`] domain.
-    pub static HANDLE: Handle = GLOBAL.register();
-}
--- hp-sharp/src/crcu/mod.rs
+++ hp-sharp/src/crcu/mod.rs
@@ -1,13 +1,11 @@
 //! A *Crash-Optimized RCU*.
 
-mod default;
 mod epoch;
 mod global;
 mod guard;
 mod local;
 mod recovery;
 
-pub use default::*;
 pub use epoch::*;
 pub use global::*;
 pub use guard::*;
