cc7aac8b7594c69471e3f902a41bc1a4dcdebed7
Jeonghyeon Kim <jeonghyeon.kim@cp.kaist.ac.kr>
2023-08-05 10:49:48 +0000

    Update archive.sh

--- archive.sh
+++ archive.sh
@@ -2,11 +2,13 @@
 
 git submodule update --init --recursive
 cargo clean
+cd cdrc-rs; cargo clean; cd ..
 cd crossbeam-ebr; cargo clean; cd ..
 cd crossbeam-pebr; cargo clean; cd ..
-cd cdrc-rs; cargo clean; cd ..
 cd hp_pp; cargo clean; cd ..
+cd hp-sharp; cargo clean; cd ..
 cd nbr-rs; cargo clean; cd ..
+cd vbr-rs; cargo clean; cd ..
 
 git-archive-all smr-benchmark.zip
 
