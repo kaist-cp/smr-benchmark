6628ed5e911aa6117b70ad937e535135720f727b
Jeonghyeon Kim <jeonghyeon.kim@cp.kaist.ac.kr>
2023-11-12 07:26:32 +0000

    Update bench scripts

--- bench-scripts/circ/bench.py
+++ bench-scripts/circ/bench.py
@@ -10,6 +10,9 @@ dss = ['h-list', 'hm-list', 'hhs-list', 'hash-map', 'nm-tree', 'skip-list']
 mms_map = ['nr', 'ebr', 'hp', 'circ-ebr', 'circ-hp', 'cdrc-ebr', 'cdrc-hp']
 mms_queue = ['nr', 'ebr', 'circ-ebr', 'cdrc-ebr', 'cdrc-ebr-flush']
 i = 10
+runs = 1
+gs = [0, 1, 2]
+
 cpu_count = os.cpu_count()
 if not cpu_count or cpu_count <= 24:
     ts_map = list(map(str, [1] + list(range(4, 33, 4))))
@@ -20,15 +23,10 @@ elif cpu_count <= 64:
 else:
     ts_map = list(map(str, [1] + list(range(10, 151, 10))))
     ts_queue = list(map(str, [1, 3, 5] + list(range(10, 151, 10))))
-runs = 1
-gs = [0, 1, 2]
 
 if os.path.exists('.git'):
     subprocess.run(['git', 'submodule', 'update', '--init', '--recursive'])
 subprocess.run(['cargo', 'build', '--release'])
-
-smr_benchmark = [os.path.join(BIN_PATH, 'smr-benchmark'), '-i', str(i)]
-double_link = [os.path.join(BIN_PATH, 'double_link'), '-i', str(i)]
 
 def key_ranges(ds):
     if ds in ["h-list", "hm-list", "hhs-list"]:
@@ -37,9 +35,6 @@ def key_ranges(ds):
         # 100K and 100M
         return ["100000", "100000000"]
 
-def opts(ds, mm, g, t, kr):
-    return ['-d', ds, '-m', mm, '-g', str(g), '-t', t, '-r', str(kr), '-o', os.path.join(RESULTS_PATH, f'{ds}.csv')]
-
 def invalid(mm, ds, g):
     is_invalid = False
     if ds == 'hhs-list':
@@ -47,6 +42,12 @@ def invalid(mm, ds, g):
     if mm == 'hp':
         is_invalid |= ds in ["h-list", "hhs-list", "nm-tree"]
     return is_invalid
+
+def extract_interval(cmd):
+    for i in range(len(cmd)):
+        if cmd[i] == '-i' and i + 1 < len(cmd):
+            return int(cmd[i + 1])
+    return 10
 
 cmds = []
 
@@ -57,16 +58,16 @@ for ds in dss:
                 continue
             for t in ts_map:
                 for kr in key_ranges(ds):
-                    cmd = smr_benchmark + opts(ds, mm, g, t, kr)
+                    cmd = [os.path.join(BIN_PATH, mm), '-i', str(i), '-d', ds, '-g', str(g), '-t', t, '-r', str(kr), '-o', os.path.join(RESULTS_PATH, f'{ds}.csv')]
                     cmds.append(cmd)
 
 for mm in mms_queue:
     for t in ts_queue:
-        cmd = double_link + ['-m', str(mm), '-t', str(t), '-o', os.path.join(RESULTS_PATH, 'double-link.csv')]
+        cmd = [os.path.join(BIN_PATH, "double_link"), '-i', str(i), '-t', str(t), '-o', os.path.join(RESULTS_PATH, 'double-link.csv')]
         cmds.append(cmd)
 
 print('number of configurations: ', len(cmds))
-print('estimated time: ', (len(cmds) * i * 1.1) // 60, ' min *', runs, 'times')
+print('estimated time: ', sum(map(extract_interval, cmds)) // 60, ' min *', runs, 'times')
 
 os.makedirs(RESULTS_PATH, exist_ok=True)
 failed = []
@@ -74,7 +75,7 @@ for run in range(runs):
     for i, cmd in enumerate(cmds):
         print("run {}/{}, bench {}/{}: '{}'".format(run + 1, runs, i + 1, len(cmds), ' '.join(cmd)))
         try:
-            subprocess.run(cmd, timeout=40)
+            subprocess.run(cmd, timeout=extract_interval(cmd) + 30)
         except subprocess.TimeoutExpired:
             print("timeout")
             failed.append(' '.join(cmd))
